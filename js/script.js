1ec5e8407d80989f34d25a024ac27f93a3fd34d8
rameshNunna <ramesh.wwwdesign@gmail.com>
2024-09-13 11:51:38 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+// page scroll
+function scrollTrigger(selector, options = {}){
+    let els = document.querySelectorAll(selector)
+    els = Array.from(els)
+    els.forEach(el => {
+        addObserver(el, options)
+    })
+}
+
+function addObserver(el, options){
+    if(!('IntersectionObserver' in window)){
+        if(options.cb){
+            options.cb(el)
+        }else{
+            entry.target.classList.add('active')
+        }
+        return
+    }
+    let observer = new IntersectionObserver((entries, observer) => { //this takes a callback function which receives two arguments: the elemts list and the observer instance
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                if(options.cb){
+                    options.cb(el)
+                }else{
+                    entry.target.classList.add('active')
+                }
+                observer.unobserve(entry.target)
+            }
+        })
+    }, options)
+    observer.observe(el)
+}
+// Example usages:
+scrollTrigger('.intro-text')
+
+scrollTrigger('.scroll-reveal', {
+    rootMargin: '-200px',
+})
+
+scrollTrigger('.loader', {
+    rootMargin: '-200px',
+    cb: function(el){
+        el.innerText = 'Loading...'
+        setTimeout(() => {
+            el.innerText = 'Task Complete!'
+        }, 1000)
+    }
+})
